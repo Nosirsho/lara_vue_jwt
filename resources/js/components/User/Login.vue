94cf697bca09318620b135b8db2d8328ece9df2c
Nosirsho <noisershoh@gmail.com>
2023-12-21 17:50:40 +0500

    Add login method to Login Component

--- resources/js/components/User/Login.vue
+++ resources/js/components/User/Login.vue
@@ -1,20 +1,35 @@
 <template>
     <div class="w-25">
         <div>
-            <input type="email" placeholder="email" class="mb-3 mt-3 form-control">
+            <input v-model="email" type="email" placeholder="email" class="mb-3 mt-3 form-control">
         </div>
         <div>
-            <input type="password" placeholder="password" class="mb-3 form-control">
+            <input v-model="password" type="password" placeholder="password" class="mb-3 form-control">
         </div>
         <div>
-            <input type="submit" value="Login" class="mb-3 btn btn-primary">
+            <input type="submit" @click.prevent="login" value="Login" class="mb-3 btn btn-primary">
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
+    name: "Login",
+
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        login() {
+            axios.post('/api/auth/login', {email: this.email, password: this.password})
+                .then(res => {
+                    localStorage.acces_token = res.data.access_token
+                })
+        }
+    }
 }
 </script>
 
--- routes/api.php
+++ routes/api.php
@@ -14,19 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-    Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
-        Route::get('/', 'IndexController');
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
+            Route::get('/', 'IndexController');
+        });
     });
 });
 
